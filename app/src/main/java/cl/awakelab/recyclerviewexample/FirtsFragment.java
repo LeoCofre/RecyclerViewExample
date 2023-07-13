2daf7d346f8d76e65d76f741cb9ccb43ef7e02e3
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-07-13 01:16:53 -0400

    Creación de fragments y clase Adapter

--- app/src/main/java/cl/awakelab/recyclerviewexample/FirtsFragment.java
+++ app/src/main/java/cl/awakelab/recyclerviewexample/FirtsFragment.java
@@ -8,15 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirtsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FirtsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -25,18 +19,10 @@ public class FirtsFragment extends Fragment {
     private String mParam2;
 
     public FirtsFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirtsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FirtsFragment newInstance(String param1, String param2) {
         FirtsFragment fragment = new FirtsFragment();
         Bundle args = new Bundle();
